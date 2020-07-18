6b2bc9530a79e4c0e1ec08d43c6d33bf6b3a29b4
Tatsuro <tatsurow1122@gmail.com>
2020-07-18 19:51:24 +0900

    exception handling

--- lib/exception_handling.rb
+++ lib/exception_handling.rb
@@ -173,3 +173,37 @@ end
 puts to_date('2017-01-01')
 
 puts to_date('dbc')
+
+def fizz_buzz(n)
+  begin
+    if n % 15 == 0
+      'Fizz Buzz'
+    elsif n % 3 == 0
+      'Fizz'
+    elsif n % 5 == 0
+      'Buzz'
+    else
+      n.to_s
+    end
+  rescue => e
+    puts "#{e.class} #{e.message}"
+  end
+end
+
+puts fizz_buzz(nil)
+
+def fizz_buzz(n)
+  if n % 15 == 0
+    'Fizz Buzz'
+  elsif n % 3 == 0
+    'Fizz'
+  elsif n % 5 == 0
+    'Buzz'
+  else
+    n.to_s
+  end
+rescue => e
+  puts "#{e.class} #{e.message}"
+end
+
+puts fizz_buzz(nil)
